5dd61c26d60087c5f7686d8a08982b73a073426a
selenyel <selenyelyazilim@gmail.com>
2023-07-03 16:53:41 +0300

    Lesson 139 - Router
    Routing, paths, query parameters, fragments, routerLink, routerLinkActive, routerLinkActiveOptions, navigate, subscribe, unsubscribe,
    
    ActivatedRoute, Params,Subscription

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -7,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  result = 0;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    }
+    this.paramsSubscription = this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
+      // this is an observable and we just subscribed to it
+      // this would help us to catch the changes in name and id when it happens!
+  }
+  onRouterLinkActive(eve){
+    this.result++;
+    console.log(eve)
   }
 
+  ngOnDestroy(){
+    this.paramsSubscription.unsubscribe();
+  }
+  // This method is for canceling the subscription after the component is destroyed
+  // If we dont do this, then our component would get destroyed but the subscription
+  // Would be active
 }
