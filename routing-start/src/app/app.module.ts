b4db32df15de380a8d4596d802e2504f7055e227
selenyel <selenyelyazilim@gmail.com>
2023-07-03 17:40:11 +0300

    Lesson 142
    child routes, conditional editing, edit page navigation & it's button, dynamic queryParams,
    displaying child routes with <router-outlet>

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -15,11 +15,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes : Routes = [
   {path:'', component: HomeComponent},
-  {path:'users', component: UsersComponent},
-  {path: 'users/:id/:name', component: UserComponent},
-  {path:'servers', component: ServersComponent},
-  {path:'servers/:id/edit', component: EditServerComponent},
-  {path:'servers/:id', component: ServerComponent}
+  {path:'users', component: UsersComponent, children: [
+    {path: ':id/:name', component: UserComponent},
+  ]},
+  {path:'servers', component: ServersComponent, children: [
+    {path:':id/edit', component: EditServerComponent},
+    {path:':id', component: ServerComponent}
+  ]},
 ];
 
 @NgModule({
