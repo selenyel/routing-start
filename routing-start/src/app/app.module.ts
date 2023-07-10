27425e89788a6b17b75e9cf70bcdf3a37be973f6
selenyel <selenyelyazilim@gmail.com>
2023-07-10 16:49:00 +0300

    Lesson 151
    Added our new services and components:
    services:
    AuthService, AuthGuard, CanDeactivateGuard
    and
    component:
    ErrorPageComponent.
    
    Added our canDeactivate:[CanDeactivateGuard] to our edit-server component. This will take its required method canDeactivate as hinted in the app.routing.module.ts and CanDeactivateGuard class which implements CanDeactivate and returns our custom CanComponentDeactivate.
    
    ^This will help us to make sure of that our user actually wants to change their current page while they have unsaved changes in the current one.
    We are achieving this by popping a confirmation bubble that asks 'Do you want to discard the changes?'
    
    We also added a simple login/logout methods in our home component which uses our authService. (It simply changes a local variable true to false or vice versa)

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -13,6 +13,10 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app.routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-activate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
@@ -24,13 +28,14 @@ import { AppRoutingModule } from './app.routing.module';
     EditServerComponent,
     ServerComponent,
     PageNotFoundComponent,
+    ErrorPageComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
--- routing-start/src/app/app.routing.module.ts
+++ routing-start/src/app/app.routing.module.ts
@@ -7,20 +7,49 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { ServerComponent } from "./servers/server/server.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { Routes, RouterModule } from "@angular/router";
+import { AuthGuard } from "./auth-guard.service";
+import { CanDeactivateGuard } from "./servers/edit-server/can-activate-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 
 const appRoutes : Routes = [
-    {path:'', component: HomeComponent},
-    {path:'users', component: UsersComponent, children: [
-      {path: ':id/:name', component: UserComponent},
-    ]},
-    {path:'servers', component: ServersComponent, children: [
-      {path:':id/edit', component: EditServerComponent},
-      {path:':id', component: ServerComponent}
-    ]},
-    {path:'not-found', component: PageNotFoundComponent},
-    {path:'**', redirectTo: '/not-found', pathMatch:'full'},
-  ];
-  
+    {
+        path:'', 
+        component: HomeComponent
+    },
+    {
+        path:'users', 
+        component: UsersComponent, 
+        children: [
+            {path: ':id/:name', component: UserComponent},
+        ]
+    },
+    {
+        path:'servers', 
+        // canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: ServersComponent, 
+        children: [
+            {path:':id/edit', component: EditServerComponent, canDeactivate:[CanDeactivateGuard]},
+            {path:':id', component: ServerComponent}
+        ]
+    },
+    {   
+        path:'not-found', 
+        component: ErrorPageComponent,
+        data: {message: 'Page not found!'}
+    },
+    {
+        path:'**', 
+        redirectTo: '/not-found', 
+        pathMatch:'full'
+    },
+];
+// If we want to protect my route I need to use canActivate
+// If we want to protect my childs but not the route itself we need to use canActivateChild
+// We could just add canActivate to each child too but it would take so much time and
+// concentration to do that since our child count may grow a lot!
+
+
 @NgModule({
     imports: [
         RouterModule.forRoot(appRoutes)
